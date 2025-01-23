5a9906ea813b3a3e1818d22757319c12729b7314
camilo365 <juniorcamiloco0124@gmail.com>
2025-01-23 13:29:45 -0500

    se agrega ambiente .env

--- practica_node/server.js
+++ practica_node/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const bodyParser = require('body-parser')
+require('dotenv').config();
 
 const app = express()
 const port = 3000;
@@ -10,8 +11,6 @@ app.use(bodyParser.json())
 
 const usuario = 'admin'
 const contra = 12345
-
-
 
 app.post('/api/crear-usuarios', (req, res) => {
     const { User, Password } = req.body;
@@ -23,6 +22,8 @@ app.post('/api/crear-usuarios', (req, res) => {
         res.json({message : "D"})
     }
 })
+const { datosbd } = require('./databases/db')
+datosbd()
 
 app.listen(port, () => {
     console.log(`Servidor de Node.js corriendo en http://localhost:${port}`);
